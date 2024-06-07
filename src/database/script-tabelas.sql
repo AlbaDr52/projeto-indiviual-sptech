585b9ae83ceb930b84907fc9056194c4d6d00f69
AlbaDr52 <pablo.santana@sptech.school>
2024-06-06 22:41:37 -0300

    finalização geral

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,8 +1,7 @@
-drop database alba;
+CREATE DATABASE albaTeste;
 
-CREATE DATABASE alba;
+USE albaTeste;
 
-USE alba;
 
 
 
@@ -10,31 +9,23 @@ CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50) unique,
-	senha VARCHAR(50)
+	senha VARCHAR(255)
 );
-
-
 
 create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	id INT AUTO_INCREMENT,
 	reacao decimal(4,3),
 	momento DATETIME,
-	fk_usuario INT,
+	fk_usuario INT not null,
+    primary key (id, fk_usuario),
 	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
 );
+insert into usuario (id, nome, email, senha)
+values (1, 'pablo', 'pablo@gmail.com', sha2('pablo',256));
 
-insert into usuario (nome, email, senha) values 
-('pablo','pablo','pablo');
-insert into medida (reacao, momento, fk_usuario) values
-('0.300',now(),'1');
-insert into medida (reacao, momento, fk_usuario) values
-('0.350',now(),'1');
-insert into medida (reacao, momento, fk_usuario) values
-('0.200',now(),'1');
-insert into medida (reacao, momento, fk_usuario) values
-('0.320',now(),'1');
-insert into medida (reacao, momento, fk_usuario) values
-('0.333',now(),'1');
-select * from medida;
 select * from usuario;
 
+insert into medida (reacao, momento, fk_usuario)
+values (0.250, now(), 1);
+
+select * from medida;
